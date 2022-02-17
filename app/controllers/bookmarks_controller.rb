b6b44655f73545d41bc4a023f9da13d3a001c87f
Svetoslav Pachev <svetoslavpachevvv@gmail.com>
2022-02-17 16:41:03 +0100

    bookmarks contoller and view

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,24 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark = Bookmark.new(strong_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+    @bookmark.save
+    redirect_to list_path(@list)
+  end
+
+
+private
+
+def strong_params
+  params.require(:bookmark).permit(:comment)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :show, :new, :create] do resources :bookmarks, only: [:new, :create, :delete]
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create, :delete]
   end
 end
