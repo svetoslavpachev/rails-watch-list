ce648e63e5c24c7ec1eb969dc94867419bc1b146
Svetoslav Pachev <svetoslavpachevvv@gmail.com>
2022-02-17 15:33:27 +0100

    bookmars controller

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,29 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(strong_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:list).permit(:name)
+  end
+end
